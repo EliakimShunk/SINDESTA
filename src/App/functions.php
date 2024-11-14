2eadc7fc00a6db241c1b95d82eebcb9d535312cb
EliakimShunk <shunkeliakim@gmail.com>
2024-11-14 12:37:48 -0300

    manha 1411

---
+++ src/App/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use JetBrains\PhpStorm\NoReturn;
+
+#[NoReturn] function dd(mixed $value) : void {
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+    die();
+}
